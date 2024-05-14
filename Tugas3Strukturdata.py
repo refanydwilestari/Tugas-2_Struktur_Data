9b3d7ef7d4af631fa23c681f19cadcdd4319dd93
refanydwi <146598703+refanydwilestari@users.noreply.github.com>
2024-05-14 20:27:37 +0700

    Update Tugas3Strukturdata.py

--- Tugas3Strukturdata.py
+++ Tugas3Strukturdata.py
@@ -1,1 +1,40 @@
-
+Anggota Kelompok 3 kelas E 20023
+1. Ronald  Budi Abdul Wahid (23091397142)
+2. M.Inbu Nadhif A.(23091397161)
+3. Refany Dwi Lestari(23091397170)
+class Map():
+    def __init__(self):
+        self.daftarKota = {}
+        self.jumlahKota = 0
+    
+    def tampilkanPeta(self):
+        for kota in self.daftarKota:
+            print(f"{kota}")
+            for kotaSebelah, jarak in self.daftarKota[kota].items():
+                print(f"\t--> {jarak} km -- {kotaSebelah}")
+        print(f"Jumlah Kota: {self.jumlahKota}")
+    
+    def tambahkanKota(self, kota):
+        if kota not in self.daftarKota:
+            self.daftarKota[kota] = {}
+            self.jumlahKota += 1
+    
+    def tambahkanJalan(self, kota1, kotaKota):
+        for kota in kotaKota:
+            if kota1 in self.daftarKota and kota in self.daftarKota:
+                self.daftarKota[kota1][kota] = kotaKota[kota]
+                self.daftarKota[kota][kota1] = kotaKota[kota]
+    
+    def hapusKota(self, kotaDihapus):
+        if kotaDihapus in self.daftarKota:
+            for kota in self.daftarKota:
+                if kotaDihapus in self.daftarKota[kota]:
+                    del self.daftarKota[kota][kotaDihapus]
+            del self.daftarKota[kotaDihapus]
+            self.jumlahKota -= 1
+    
+    def hapusJalan(self, kota1, kota2):
+        if kota1 in self.daftarKota and kota2 in self.daftarKota[kota1]:
+            del self.daftarKota[kota1][kota2]
+            del self.daftarKota[kota2][kota1]
+          
